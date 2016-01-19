2e740362125b15e4b748e6108d0b737e24657360
AxelAVH <axel.avh@gmx.de>
2016-01-19 10:38:42 +0100

    .

--- money/de/amo/money/Kategoriefacade.java
+++ money/de/amo/money/Kategoriefacade.java
@@ -22,16 +22,16 @@ public class Kategoriefacade {
         return instance;
     }
 
-    private Map<String,String> kategorien;
+    private List<Kategorie> kategorien;
 
-    public Map<String, String> getKategorien() {
+    public List <Kategorie> getKategorien() {
         return kategorien;
     }
 
     public void init() {
-        kategorien = new HashMap<String,String>();
+        kategorien = new ArrayList<>();
 
-        if (true) {
+//        if (true) {
 
             try {
                 loadKategorien();
@@ -39,50 +39,56 @@ public class Kategoriefacade {
                 e.printStackTrace();
             }
             return;
+//        }
+
+//        kategorien.add(new Kategorie("AK", "Auto - Kraftstoff"));
+//        kategorien.add(new Kategorie("AR", "Auto - alles ausser Kraftstoff/Steuer/Versicherung"));
+//        kategorien.add(new Kategorie("AV", "Auto - Versicherung"));
+//        kategorien.add(new Kategorie("AS", "Auto - Steuern"));
+//        kategorien.add(new Kategorie("BE", "Bar - Entnahme"));
+//        kategorien.add(new Kategorie("ES", "Einkommen - Steuer" ));
+//        kategorien.add(new Kategorie("EK", "Einkommen - alles rumd um dbh, auch Reisekosten und -Rückerstattungen" ));
+//        kategorien.add(new Kategorie("FC", "Freizeit - Chor"));
+//        kategorien.add(new Kategorie("GK", "Gesundheit - Kosten"));
+//        kategorien.add(new Kategorie("GR", "Gesundheit - Rückerstattungen"));
+//        kategorien.add(new Kategorie("FU", "Freizeit - Urlaub/Kultur"));
+//        kategorien.add(new Kategorie("LT", "Leben - täglicher Bedarf" ));
+//        kategorien.add(new Kategorie("LG", "Leben - Geschenke u. sonstige Anschaffungen" ));
+//        kategorien.add(new Kategorie("LK", "Leben - Kleidung" ));
+//        kategorien.add(new Kategorie("WM", "Wohnen - Miete"));
+//        kategorien.add(new Kategorie("WW", "Wohnen - Warm"));
+//        kategorien.add(new Kategorie("WT", "Wohnen - Telekommunikation"));
+//        kategorien.add(new Kategorie("VB", "Versicherung - Berufsunfähigkeit"));
+//        kategorien.add(new Kategorie("VR", "Versicherung - Rente"));
+//        kategorien.add(new Kategorie("UA", "Unterhalt - Aktivitäten/Geschenke Kinder"));
+//        kategorien.add(new Kategorie("UF", "Unterhalt - Frau"));
+//        kategorien.add(new Kategorie("UK", "Unterhalt - Kinder"));
+//        kategorien.add(new Kategorie("US", "Unterhalt - Sonstiges"));
+//        kategorien.add(new Kategorie("ZK", "Zuschüsse - Kleinsorge"));
+//        kategorien.add(new Kategorie("ZM", "Zuschüsse - Möller W."));
+    }
+
+    public String getKategoriebezeichnung(String code) {
+        for (Kategorie kategorie : kategorien) {
+            if (kategorie.getCode().equals(code)) {
+                return kategorie.getBeschreibung();
+            }
         }
 
-        kategorien.put("AK", "Auto - Kraftstoff");
-        kategorien.put("AR", "Auto - alles ausser Kraftstoff/Steuer/Versicherung");
-        kategorien.put("AV", "Auto - Versicherung");
-        kategorien.put("AS", "Auto - Steuern");
-        kategorien.put("BE", "Bar - Entnahme");
-        kategorien.put("ES", "Einkommen - Steuer" );
-        kategorien.put("EK", "Einkommen - alles rumd um dbh, auch Reisekosten und -Rückerstattungen" );
-        kategorien.put("FC", "Freizeit - Chor");
-        kategorien.put("GK", "Gesundheit - Kosten");
-        kategorien.put("GR", "Gesundheit - Rückerstattungen");
-        kategorien.put("FU", "Freizeit - Urlaub/Kultur");
-        kategorien.put("LT", "Leben - täglicher Bedarf" );
-        kategorien.put("LG", "Leben - Geschenke u. sonstige Anschaffungen" );
-        kategorien.put("LK", "Leben - Kleidung" );
-        kategorien.put("WM", "Wohnen - Miete");
-        kategorien.put("WW", "Wohnen - Warm");
-        kategorien.put("WT", "Wohnen - Telekommunikation");
-        kategorien.put("VB", "Versicherung - Berufsunfähigkeit");
-        kategorien.put("VR", "Versicherung - Rente");
-        kategorien.put("UA", "Unterhalt - Aktivitäten/Geschenke Kinder");
-        kategorien.put("UF", "Unterhalt - Frau");
-        kategorien.put("UK", "Unterhalt - Kinder");
-        kategorien.put("US", "Unterhalt - Sonstiges");
-        kategorien.put("ZK", "Zuschüsse - Kleinsorge");
-        kategorien.put("ZM", "Zuschüsse - Möller W.");
+        return null;
     }
 
-    public String getKategoriebezeichnung(String kategorie) {
-        return kategorien.get(kategorie);
-    }
-
-    public List<String> getKategorieCodes() {
-        List<String> ret =new ArrayList<>();
-        ret.addAll(kategorien.keySet());
-        return ret;
-    }
+//    public List<String> getKategorieCodes() {
+//        List<String> ret =new ArrayList<>();
+//        ret.addAll(kategorien.keySet());
+//        return ret;
+//    }
 
     public List<String> getComboboxList() {
         SortedSet<String> set = new TreeSet<String>();
 
-        for (Map.Entry<String, String> entry : kategorien.entrySet()) {
-            set.add(entry.getKey() + " - " + entry.getValue());
+        for (Kategorie kategorie : kategorien) {
+            set.add(kategorie.getCode() + " - " + kategorie.getBeschreibung());
         }
 
         ArrayList ret = new ArrayList();
@@ -107,34 +113,42 @@ public class Kategoriefacade {
             e.printStackTrace();
         }
 
-        kategorien = new HashMap<>();
+        kategorien = new ArrayList<>();
 
         Preferences kategorieNode = prefs.node("Kategorien");
 
         String[] keys = kategorieNode.keys();
 
+        SortedMap<String, Kategorie> map = new TreeMap<>();
+
         for (int i = 0; i < keys.length; i++) {
-            String key = keys[i];
-            String value = kategorieNode.get(key, "unbekannt");
-            kategorien.put(key, value);
+            Kategorie kategorie = new Kategorie(keys[i], kategorieNode.get(keys[i], "unbekannt"));
+            map.put(kategorie.getCode(),kategorie);
         }
 
-
+        kategorien.addAll(map.values());
     }
 
     public void saveKategorien() throws BackingStoreException {
 
         Preferences prefs = Preferences.userNodeForPackage(this.getClass());
 
+        // ToDo: was muss man wirklich machen, um den Knoten zu töten?
+        prefs.clear();
         prefs.remove("Kategorien");
         Preferences katPrefs = prefs.node("Kategorien");
-
-        for (Map.Entry<String, String> entrySet: kategorien.entrySet()) {
-            katPrefs.put(entrySet.getKey(), entrySet.getValue());
-        }
-
+        katPrefs.removeNode();
         prefs.flush();
 
+        katPrefs = prefs.node("Kategorien");
+
+        for (Kategorie kategorie : kategorien) {
+            if (kategorie.getCode() == null || "".equals(kategorie.getCode().trim())) {
+                continue;
+            }
+            katPrefs.put(kategorie.getCode(), kategorie.getBeschreibung());
+        }
+        prefs.flush();
     }
 
 }
